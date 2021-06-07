86dc8f09dd9990b2edbec8b624f0ab776286764a
Ester Ribeiro dos Santos <ester.santos@teadit.com>
2021-06-06 23:26:58 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using Shop.Repositories;
+using Shop.Services;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("autheticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado 0 {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee, manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            //recupera usuário
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+                return HttpStatusCode.NotFound;
+
+            //gera o token
+            var token = TokenService.GenerateToken(user);
+
+            //oculta a senha
+            user.Password = "";
+
+            //retorna os dados
+            return new
+            {
+                User = user,
+                Token = token
+            };
+
+        }
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Repositories
+{
+    public class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(
+                new User
+                {
+                    Id = 1,
+                    Username = "ester",
+                    Password = "456",
+                    Role = "manager"
+                });
+            users.Add(
+                new User
+                {
+                    Id = 1,
+                    Username = "joao",
+                    Password = "123",
+                    Role = "employee"
+                });
+
+            return users.Where(x => x.Username.ToLower() == username.ToLower() && x.Password == x.Password).FirstOrDefault();
+        }
+    }
+}
